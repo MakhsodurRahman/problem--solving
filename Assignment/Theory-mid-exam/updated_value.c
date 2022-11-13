da201734f6f71f9cb51efa5cea3214590ffb16f6
Makhsodur-Rahman <makhsodurrahmanhridoy@gmail.com>
2022-11-13 23:40:06 +0600

    updated value

---
+++ Assignment/Theory-mid-exam/updated_value.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int n = 10;
+    int x = 5;
+    int i;
+    int index = 0;
+    int a[] = {12, 7, 3, 71, 2, 43, 38, 23, 45, 22};
+    int b[n];
+    for(i=0; i<n; i++)
+    {
+        b[i] = a[i] + 5;
+    }
+    for(i = 0; i<n; i++)
+    {
+        printf("%d ",b[i]);
+    }
+
+}
