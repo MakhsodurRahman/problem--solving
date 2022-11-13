3cfbab34f552f99cafdf2117ecf3fa14020dc248
Makhsodur-Rahman <makhsodurrahmanhridoy@gmail.com>
2022-11-13 23:38:15 +0600

    ascii value

---
+++ Assignment/Theory-mid-exam/Ascii_value.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    char a[10];
+	a[0] = 98;
+	a[1] = 97;
+	a[2] = 'n';
+	a[3] = 'a';
+	a[4] = 'n';
+	a[5] = 'a';
+	a[6] = '\0';
+	for(int i = 0; i<7; i++){
+        printf("%c",a[i]);
+	}
+}
+
